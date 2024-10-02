be868128266c68bd1f6dd3fbe416e401483fd5d6
Kyle Danielle Dioso <kyledanielledioso@gmail.com>
2024-10-02 13:09:44 +0800

    1002_Vid25_Queues-are-Easier-Than-You-Think

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
-    Illuminate\Support\Facades\Mail::to('jeffrey@laracasts.com')->send(
-        new \App\Mail\JobPosted()
-    );
+    $job = Job::first();
+
+    TranslateJob::dispatch($job);
 
     return 'Done';
 });
