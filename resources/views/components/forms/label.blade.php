5d8b3b63c5f89eb5995518ce669d4dccc4e9fa2c
Kyle Danielle Dioso <kyledanielledioso@gmail.com>
2024-11-15 22:28:08 +0800

    1115_Vid30_Wrap-Up

--- resources/views/components/forms/label.blade.php
+++ resources/views/components/forms/label.blade.php
@@ -1,5 +1,5 @@
+@props(['name', 'label'])
 <div class="inline-flex items-center gap-x-2">
     <span class="w-2 h-2 bg-white inline-block"></span>
-    
-    <h3 class="font-bold text-lg">{{ $slot }}</h3>
+    <label class="font-bold" for="{{ $name }}">{{ $label }}</label>
 </div>
